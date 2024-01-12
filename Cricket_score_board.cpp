08c66470ea8619c52c18c30f836741d593bd7340
2021331033ar <137720672+2021331033ar@users.noreply.github.com>
2024-01-12 11:09:38 +0600

    Update Cricket_score_board.cpp

--- Cricket_score_board.cpp
+++ Cricket_score_board.cpp
@@ -1,299 +1,251 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-void createHTML();
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 // Global Vars
-
-int run =0;
+int run = 0;
 int over = 0;
-int ball =0;
+int ball = 0;
 int wicket = 0;
 
-char content[255] =  " ";
+void printLiveScore() {
+    printf("\nLive Score:\n");
+    printf("Score: %d / %d\n", run, wicket);
+    printf("Overs: %d.%d\n", over, ball);
 
-char scoreStr[4];
-char wicketStr[2];
-char overStr[100];
-char ballStr[1];
-
-void createHTML(){
-    FILE *pFile = fopen("score.html", "w");
-    fprintf(pFile, "<html><body><h3>");
-    fprintf(pFile, content);
-    fprintf(pFile, "</h3>");
-    fprintf(pFile, "</body></html>");
-    fclose(pFile);
 }
 
-int main(){
-    int score;
+int main() {
+    int score, w, o;
+    printf("Enter overs and wickets: ");
+    scanf("%d%d", &o, &w);
+    printf("%d overs & %d wickets", o, w);
+    while (1) {
+       //printLiveScore();
 
-    while(true){
-        printf("\n Enter Score : ");
+        printf("\n Show live score, '0' press again ");
         scanf("%d", &score);
         getchar();
+          // Consume the newline character left in the buffer
 
-        switch(score){
-            case 0 : {
-                char dot;
-                printf("Enter Type of Dot Ball \n");
-                printf(" 'W' for a wicket \n 'D' for a dot Ball\n");
-                scanf("%c", &dot);
-                switch(dot){
-                    case 'W' : {
-                        wicket +=1;
-                        ball +=1;
-                        printf("We have a wicket\n");
+           srand(time(NULL));
+                int random = (rand() % 7) + 1 ;
+
+        switch (random) {
+            case 0: {
+                 srand(time(NULL));
+                int random = (rand() % 2) ;
+                switch (random) {
+                    case 0: {
+                        wicket += 1;
+                        ball += 1;
+                        printf("\nWe have a wicket\n");
                         break;
                     }
-                    case 'D' : {
-                        ball +=1;
-                        printf("Another Dot ball \n");
+                    case 1: {
+                        ball += 1;
+                        printf("\nDot ball\n");
                         break;
                     }
                 }
                 break;
             }
 
-            case 1 : {
-                char single ;
-                printf("Enter the type of single taken \n");
-                printf(" 'S' for a Single \n 'W' for a wide ball\n 'N' for a No ball\n 'R' for a run out. \n");
-                scanf("%c", &single);
-                switch(single){
-                case 'S' : {
-                run +=1;
-                ball +=1;
-                printf("Rotating the strike \n");
-                break;
-                }
-                case 'W' : {
-                run +=1;
-                printf("Wide ball \n");
-                break;
-                }
-                case 'N' : {
-                run +=1;
-                //ball -=1;
-                printf("No ball, Free hit\n");
-                break;
-                }
-                case 'R'  : {
-                run +=1;
-                ball +=1;
-                wicket +=1;
-                printf("Wicket gone\n");
-                break;
-                }
-
-                default :
-                    printf("Invalid Input\n");
+            case 1: {
+                srand(time(NULL));
+                int random = (rand() % 4);
+                switch (random) {
+                    case 0: {
+                        run += 1;
+                        ball += 1;
+                        printf("\nSingle run\n");
+                        break;
+                    }
+                    case 1: {
+                        run += 1;
+                        printf("\nWide ball\n");
+                        break;
+                    }
+                    case 2: {
+                        run += 1;
+                        printf("\nNo ball, Free hit\n");
+                        break;
+                    }
+                    case 3: {
+                        run += 1;
+                        ball += 1;
+                        wicket += 1;
+                        printf("\nWicket gone and Single run.\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
                 break;
             }
-            case 2 : {
-                 char doubles ;
-                printf("Enter the type of double taken \n");
-                printf(" 'D' for a double \n 'W' for a wide ball\n 'N' for a No ball\n 'R' for a run out. \n");
-                scanf("%c", &doubles);
-                switch(doubles){
-                case 'D' : {
-                run +=2;
-                ball +=1;
-                printf("Rotating the strike \n");
-                break;
+            case 2: {
+                srand(time(NULL));
+                int random = (rand() % 5);
+                switch (random) {
+                    case 0: {
+                        run += 2;
+                        ball += 1;
+                        printf("\nDouble run.\n");
+                        break;
+                    }
+                    case 1: {
+                        run += 2;
+                        printf("\nWide ball and single run.\n");
+                        break;
+                    }
+                    case 2: {
+                        run += 2;
+                        printf("\nNo ball, Free hit and single run.\n");
+                        break;
+                    }
+                    case 3: {
+                        run += 2;
+                        ball += 1;
+                        wicket += 1;
+                        printf("\nWicket gone and double run.\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
-                case 'W' : {
-                run +=2;
-
-                printf("Wide ball \n");
-                break;
-                }
-                case 'N' : {
-                run +=2;
-                //ball -=1;
-                printf("No ball, Free hit\n");
-                break;
-                }
-                case 'R'  : {
-                run +=2;
-                ball +=1;
-                wicket +=1;
-                printf("Wicket gone\n");
-                break;
-                }
-
-                default :
-                    printf("Invalid Input\n");
-                }
-
                 break;
             }
-            case 3 : {
-                 char triple ;
-                printf("Enter the type of triple taken \n");
-                printf(" 'T' for a Triple \n 'W' for a wide ball\n 'N' for a No ball\n 'R' for a run out. \n");
-                scanf("%c", &triple);
-                switch(triple){
-                case 'T' : {
+          case 3 : {
+                srand(time(NULL));
+                int random = (rand() % 4);
+
+                switch(random){
+                case 0 : {
                 run +=3;
                 ball +=1;
-                printf("Rotating the strike \n");
+                printf("\n Triple run. \n");
                 break;
                 }
-                case 'W' : {
+                case 1 : {
                 run +=3;
-                printf("Wide ball \n");
+                printf("\nWide ball and double run. \n");
                 break;
                 }
-                case 'N' : {
+                case 2 : {
                 run +=3;
                 //ball -=1;
-                printf("No ball, Free hit\n");
+                printf("\nNo ball, Free hit and double run.\n");
                 break;
                 }
-                case 'R'  : {
+                case 3 : {
                 run +=3;
                 ball +=1;
                 wicket +=1;
-                printf("Wicket gone\n");
+                printf("\nWicket gone and triple run.\n");
                 break;
                 }
-
                 default :
-                    printf("Invalid Input\n");
+                    printf("\nDad Ball\n");
                 }
-
                 break;
             }
             case 4: {
-                char boundary ;
-                printf("Enter the type of boundary taken \n");
-                printf(" 'B' for a boundary \n ");
-                scanf("%c", &boundary);
-                switch(boundary){
-                case 'B' : {
-                run +=4;
-                ball +=1;
-                printf("It is a four' \n");
-                break;
+                srand(time(NULL));
+                int random = (rand() % 1);
+                switch (random) {
+                    case 0: {
+                        run += 4;
+                        ball += 1;
+                        printf("\nIt is a four'\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
-
-                default :
-                    printf("Invalid Input\n");
-                }
-
                 break;
             }
             case 5: {
-                char fiver;
-                printf("Enter the type of fiver taken \n");
-                printf(" 'W' for a wide ball\n 'N' for a No ball\n");
-                scanf("%c", &fiver);
-                switch(fiver){
-                case 'W' : {
-                run +=5;
-                printf("Wide ball \n");
-                break;
-                }
-                case 'N' : {
-                run +=5;
-               // ball -=1;
-                printf("No ball, Free hit\n");
-                break;
-                }
 
-                default :
-                    printf("Invalid Input\n");
+                srand(time(NULL));
+                int random = (rand() % 2);
+                switch (random) {
+                    case 0: {
+                        run += 5;
+                        printf("\nWide ball and four.\n");
+                        break;
+                    }
+                    case 1: {
+                        run += 5;
+                        printf("\nNo ball, Free hit and four.\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
-
                 break;
             }
             case 6: {
-                 char sixer ;
-                printf("Enter the type of sixer taken \n");
-                printf(" 'S' for a Sixer\n");
-                scanf("%c", &sixer);
-                switch(sixer){
-                case 'S' : {
-                run +=6;
-                ball +=1;
-                printf("It is a Six\n");
-                break;
+
+                srand(time(NULL));
+                int random = (rand() % 1);
+                switch (random) {
+                    case 0: {
+                        run += 6;
+                        ball += 1;
+                        printf("\nIt is a Six\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
-                default :
-                    printf("Invalid Input\n");
+                break;
+            }
+            case 7: {
+
+                srand(time(NULL));
+                int random = (rand() % 1);
+                switch (random) {
+                    case 0: {
+                        run += 7;
+                        printf("\nNo ball, Free hit and six.\n");
+                        break;
+                    }
+                    default:
+                        printf("\nDad Ball\n");
                 }
                 break;
             }
-
-          case 7: {
-              char seven ;
-                printf("Enter the type of seven taken \n");
-                printf("  'N' for No Ball\n");
-                scanf("%c", &seven);
-                switch(seven){
-                case 'N' : {
-                run +=7;
-               //ball -=1;
-                printf("No ball, Free hit\n");
-                break;
-                }
-
-                default :
-                    printf("Invalid Input\n");
-                }
-
-                break;
-            }
-
             case 400: {
                 exit(1);
                 // Exit code
                 break;
             }
-            default : {
-            printf("This is an invalid input\n");
+            default: {
+                printf("\nThis is an invalid input\n");
             }
         }
 
-       itoa(run, scoreStr, 10);
-       itoa(wicket, wicketStr, 10);
-       itoa(over, overStr, 10);
-       itoa(ball, ballStr, 10);
+        if (ball == 6) {
+            over += 1;
+            ball = 0;
+        }
+        if (wicket == w) {
+                printf("\nWicket ended.\n");
+            printf("\nInnings Ended\n");
+            //printf("Target : %d",run+1);
+            break;
+        }
+        if (over == o) {
+                printf("\nOver ended.\n");
+            printf("\nInnings Ended\n");
+            //printf("Target : %d",run+1);
+            break;
+        }
+        printLiveScore();
 
-       strcat(content, "Score : ");
-       strcat(content, scoreStr);
-       strcat(content, "\n");
-       strcat(content, "/ ");
-       strcat(content, wicketStr);
-       strcat(content, "\n");
-       strcat(content, " Overs :  ");
-       strcat(content, overStr);
-       strcat(content, ".");
-       strcat(content, ballStr);
-       strcat(content, "\n");
-
-       createHTML();
-
-       if(ball == 6){
-        over +=1;
-        ball =0;
-       }
-
-       if(wicket == 10){
-        printf("Innings Ended");
-        break;
-       }
-
-       if(over == 50){
-        printf("Innings Ended");
-        break;
-       }
-
-     strcpy(content, " ");
     }
+    printLiveScore();
+
+    printf("\nTarget : %d",run+1);
 }
-//Ended
